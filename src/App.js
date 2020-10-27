5b4b855881ea5898b70a1f06acf230c76e613c1d
Nik Schaefer <nikkschaefer@gmail.com>
2020-10-26 19:01:58 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+import Home from './Pages/Home'
+import NavBar from './Pages/NavBar'
+import Footer from './Pages/Footer'
+
+import {BrowserRouter,Route,} from "react-router-dom";
+
+
+function App() {
+
+
+  
+
+
+  return (
+    <BrowserRouter>
+      
+      
+    <div className="App">
+
+      <NavBar />
+      <Route exact path='/' component={Home} />
+      <Footer />
+
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
