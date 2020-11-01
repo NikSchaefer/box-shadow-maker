d9e64c802baeffb5f254fe52784062c619d3763d
Nik Schaefer <nikkschaefer@gmail.com>
2020-10-31 22:23:49 -0500

    Decoration

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './App.css';
-
-import Home from './Pages/Home'
-import NavBar from './Pages/NavBar'
-import Footer from './Pages/Footer'
-
-import {BrowserRouter,Route,} from "react-router-dom";
-
+import github from './Images/github.svg'
+import mail from './Images/mail.svg'
 
 function App() {
 
+  const [xOffset, setXOffset] = useState(0)
+  const [yOffset, setYOffset] = useState(0)
+  const [blur, setBlur] = useState(10)
+  const [color, setColor] = useState('black')
+  const [code, setCode] = useState(null)
 
-  
-
+  const update = () => {
+    const object = document.getElementById('display');
+    console.log(color)
+    object.style.boxShadow = `${xOffset}px ${yOffset}px ${blur}px ${color}`
+    console.log(color)
+    setCode(document.getElementById('display').style.boxShadow)
+  }
+  window.onload = function () {
+    update()
+  }
+  const handleX = (e) => {
+    setXOffset(e.target.value)
+    update()
+  }
+  const handleY = (e) => {
+    setYOffset(e.target.value)
+    update()
+  }
+  const handleBlur = (e) => {
+    setBlur(e.target.value)
+    update()
+  }
+  const handleColor = (e) => {
+    setColor(e.target.value)
+    update()
+    update()
+  }
 
   return (
-    <BrowserRouter>
-      
-      
-    <div className="App">
+    <div className='main-div'>
 
-      <NavBar />
-      <Route exact path='/' component={Home} />
-      <Footer />
+      <div className="slidecontainer">
 
+        <p className='title'>Box Shadow Creator</p>
+
+        <div className='slides'>
+
+          <p className='slider-info'>X Offset: {xOffset}px</p>
+          <input onChange={handleX} defaultValue='0' type="range" min="-400" max="400" className="slider" />
+
+          <p className='slider-info'>Y Offset: {yOffset}px</p>
+          <input onChange={handleY} defaultValue='0' type="range" min="-400" max="400" className="slider" />
+
+          <p className='slider-info'>Blur: {blur}px</p>
+          <input onChange={handleBlur} defaultValue='10' type="range" min="1" max="100" className="slider" />
+
+          <p className='slider-info'>Color: {color}</p>
+          <input onChange={handleColor} className="color-input" />
+
+          <p>Copy the text below into your elements CSS</p>
+          <p className='copy-text'>{`box-shadow: ${code};`}</p>
+
+
+        </div>
+      </div>
+
+      <div className='display-div'>
+        <div id='display' />
+      </div>
+
+      <div className='footer'>
+        <a href='https://github.com/NikSchaefer/box-shadow-maker'><img alt='' src={github} /></a>
+      </div>
     </div>
-    </BrowserRouter>
   );
 }
 
